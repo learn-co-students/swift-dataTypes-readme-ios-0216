1a8ea75ec099c0c9d25547dc9d666974d26f4b7f
ArielSD <ascottdicker@dosomething.org>
2016-07-18 13:39:39 -0400

    Done

---
+++ DataTypesPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+let sixthPlanet: String = "Saturn"
+let seventhPlanet = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
+
+        
